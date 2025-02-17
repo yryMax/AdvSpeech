b7da5b57556a5de233be584ff74b18bb8d11e8ab
Yang Renyi <jianghurainxwy@gmail.com>
2025-02-17 03:52:43 +0100

    .

--- synthesis/synthesizer.py
+++ synthesis/synthesizer.py
@@ -104,9 +104,9 @@ class XTTSSynthesizer(Synthesizer):
                 cwd=path,
             )
         except subprocess.CalledProcessError as e:
-            # print(f"Error: Process failed with exit code {e.returncode}.")
-            # print("Child stdout =", e.stdout)
-            # print("Child stderr =", e.stderr)
+            print(f"Error: Process failed with exit code {e.returncode}.")
+            print("Child stdout =", e.stdout)
+            print("Child stderr =", e.stderr)
             reader_should_stop.set()
             exception = True
         finally:
