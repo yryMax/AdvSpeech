453b4d2096bf18eb3318b9343d4ae3941aa74c4e
Yang Renyi <jianghurainxwy@gmail.com>
2025-02-17 02:22:56 +0100

    fix: wespeaker result diff

--- Metrics/EffectivenessMetric.py
+++ Metrics/EffectivenessMetric.py
@@ -4,13 +4,17 @@ import tempfile
 
 import torch
 import torchaudio
+import wespeaker
+
+model = wespeaker.load_model("english")
+model.set_device("cuda:0")
 
 
 def wespeaker_runner(audio1: torch.Tensor, audio2: torch.Tensor, sr):
     """
     :param audio1: audio tensor 1
     :param audio2: audio tensor 2
-    :return: we speaker score
+    :return: wespeaker score
     """
     with tempfile.NamedTemporaryFile(
         suffix=".wav", delete=True
@@ -18,25 +22,7 @@ def wespeaker_runner(audio1: torch.Tensor, audio2: torch.Tensor, sr):
         torchaudio.save(f1.name, audio1, sr, format="wav")
         torchaudio.save(f2.name, audio2, sr, format="wav")
 
-        cmd = [
-            "wespeaker",
-            "--task",
-            "similarity",
-            "--audio_file",
-            f1.name,
-            "--audio_file2",
-            f2.name,
-        ]
-
-        try:
-            result = subprocess.run(cmd, capture_output=True, text=True, check=True)
-            cleaned = re.sub(r"\x1b\[.*?m", "", result.stdout).strip()
-            similarity_score = float(cleaned)
-            return similarity_score
-
-        except subprocess.CalledProcessError as e:
-            print(f"[ERROR] WeSpeaker execution failed: {e}")
-            return None
+        return model.compute_similarity(f1.name, f2.name)
 
 
 if __name__ == "__main__":
